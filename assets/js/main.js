acb44d9571fdd18095fbe607d31306f0eb92ef89
resurtm <resurtm@gmail.com>
2017-01-13 19:08:27 +0600

    w

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,53 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 
-let app = new Vue({
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment (state) {
+            state.count++;
+        },
+        decrement (state) {
+            state.count--;
+        },
+    },
+});
+
+const Counter = {
+    computed: {
+        count () {
+            return this.$store.state.count;
+        },
+    },
+    methods: {
+        onClick() {
+            this.$store.commit('increment');
+        }
+    },
+    template: `
+<div>
+    <div>{{ count }}</div>
+    <button class="btn" @click="onClick">Click!</button>
+</div>
+`,
+};
+
+Vue.component('todo-item', {
+    props: ['task'],
+    template: `<li>{{task.text}}</li>`,
+});
+
+const app = new Vue({
     el: '#main-container',
-    data: {
-        message: 'Hello Vue!'
-    }
+    store,
+    components: {Counter},
+    template: `
+<div class="app">
+    <counter></counter>
+</div>
+`,
 });
