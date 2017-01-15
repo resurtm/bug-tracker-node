082a86f2669ffd767e5abc5f9bdc7960be79931e
resurtm <resurtm@gmail.com>
2017-01-15 18:37:46 +0600

    w

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -2,15 +2,15 @@ import dotenv from 'dotenv'
 import express from 'express'
 import nodemailer from 'nodemailer'
 import nodemailerSes from 'nodemailer-ses-transport'
+import pug from 'pug'
+import path from 'path'
 
 dotenv.config();
 
 let router = express.Router();
 
 router.post('/send', (req, res, next) => {
-    console.log(req.body);
-
-    let transporter = nodemailer.createTransport(nodemailerSes({
+    const transporter = nodemailer.createTransport(nodemailerSes({
         accessKeyId: process.env.SES_ACCESS_KEY_ID,
         secretAccessKey: process.env.SES_SECRET_ACCESS_KEY
     }));
@@ -19,18 +19,23 @@ router.post('/send', (req, res, next) => {
         from: `"${req.body.name}" <${req.body.email}>`,
         to: 'resurtm+dev@gmail.com',
         subject: 'Bug Tracker Node',
-        text: req.body.message,
-        html: req.body.message
+        text: pug.renderFile(path.join(__dirname, '..', '..', 'views', 'emails', 'contact-text.pug'), req.body),
+        html: pug.renderFile(path.join(__dirname, '..', '..', 'views', 'emails', 'contact-html.pug'), req.body)
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
+        let json;
         if (error) {
-            return console.log(error);
+            console.log('Message cannot be sent:');
+            console.log(error);
+            json = {status: 'failed', message: error.message};
+        } else {
+            console.log('Message has been sent:');
+            console.log(info);
+            json = {status: 'ok'};
         }
-        console.log('Message sent: ' + info.response);
+        res.json(json);
     });
-
-    res.json({status: 'ok'});
 });
 
 export default router
